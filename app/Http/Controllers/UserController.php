277f5784c2f09d15e9fd5ae46ff6ba9b187d0c45
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-06-12 12:59:38 +0300

    chore: handle sign in form and routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -35,55 +36,83 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        // user validation
-        $user = new User(); 
-        $validator = Validator::make($request->all(),$user->rules);
-        if ($validator->fails()) {
-            $errors = $validator->errors();
-            return response()->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
-        }
+        // check if user exist
+        $user = User::where('email', $request->email)->first();
 
-        $id = generateUniqueId("users", "user_id");
-        $newOne = User::create([
-            'user_id' =>  $id,
-            'name' =>  $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-            'photo' => $request->photo,
-            'type' => $request->type,
-        ]);
-
-        if ($request->type == 't' || $request->type == 'a') {
-            
-            // teacher validation
-            $teacher = new Teacher(); 
-            $validator = Validator::make($request->all(),$teacher->rules);
+        if ($user) {
+            // validation
+            $validator = Validator::make($request->all(), [
+                'email' => [
+                    'required',
+                    'email'
+                ],
+                'password' => [
+                    'required'
+                ]
+            ]);
             if ($validator->fails()) {
                 $errors = $validator->errors();
                 return response()->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
             }
 
-            Teacher::create([
-                'teacher_id' => $id,
-                'title' => $request->title,
-                'bio' => $request->bio,
-            ]);
-        } elseif ($request->type == 's') {
-            // student validation
-            $student = new Student(); 
-            $validator = Validator::make($request->all(),$student->rules);
+            // Attempt to authenticate the user
+            if (Auth::attempt($request->only('email', 'password'))) {
+                // Authentication successful
+                return redirect()->intended('/profile');
+            } else {
+                // Authentication failed
+                return back()->withErrors([
+                    'email' => 'Invalid credentials',
+                ]);
+            }
+        }else{
+            // validation
+            $validator = Validator::make($request->all(),(new User())->rules);
             if ($validator->fails()) {
                 $errors = $validator->errors();
                 return response()->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
             }
 
-            Student::create([
-                'student_id' => $id,
-                'fav_questions' => $request->fav_questions
+            $id = generateUniqueId("users", "user_id");
+            $newOne = User::create([
+                'user_id' =>  $id,
+                'name' =>  $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+                'photo' => $request->photo,
+                'type' => $request->type,
             ]);
-        }
 
-        return redirect('/');
+            if ($request->type == 't' || $request->type == 'a') {
+                // teacher validation
+                $teacher = new Teacher(); 
+                $validator = Validator::make($request->all(),$teacher->rules);
+                if ($validator->fails()) {
+                    $errors = $validator->errors();
+                    return response()->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
+                }
+    
+                Teacher::create([
+                    'teacher_id' => $id,
+                    'title' => $request->title,
+                    'bio' => $request->bio,
+                ]);
+            } elseif ($request->type == 's') {
+                // student validation
+                $student = new Student(); 
+                $validator = Validator::make($request->all(),$student->rules);
+                if ($validator->fails()) {
+                    $errors = $validator->errors();
+                    return response()->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
+                }
+    
+                Student::create([
+                    'student_id' => $id,
+                    'fav_questions' => $request->fav_questions
+                ]);
+            }
+            return redirect('/profile');
+        }
     }
 
     /**
