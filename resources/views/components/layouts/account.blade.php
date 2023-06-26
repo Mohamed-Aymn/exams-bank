da85690f3a2a5a84dc954cdcbdd7888e2612267f
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-06-26 15:57:38 +0300

    chore: edit layout blades

--- resources/views/components/layouts/account.blade.php
+++ resources/views/components/layouts/account.blade.php
@@ -6,45 +6,19 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <title>Exams Bank</title>
-
     @include('../partials.styles')
 </head>
 <body class="antialiased text-gray-700 bg-gray-100">
 
-@if ($showHeader)
-    @include('../components.organisms.header')
-@endif
+@include('../components.organisms.header')
 
 <main id='app' class="flex flex-no-wraph h-[calc(100vh-80px)]">
-
-    @yield('sidebar')
-    {{-- <div class="absolute flex-col justify-between hidden w-64 shadow bg-gray-50 sm:relative md:h-full sm:flex">
-        <div class="px-8 mt-12">
-            <div class="text-gray-400">Content</div>
-
-            <x-button :isAnchor="true" format="tertiary" class="ml-2">
-                State 1
-            </x-button>
-        </div>
-    </div> --}}
-
-    {{-- content --}}
     <div class="container w-11/12 h-64 px-6 py-10 mx-auto md:w-4/5">
         @yield('content')
     </div>
-
-
-    {{-- small screens bottom-nav --}}
-    @yield('mobile-bottom-nav')
-    {{-- <div class="absolute bottom-0 flex justify-center w-full gap-2 py-2 border-t border-gray-200 items-top bg-gray-50 sm:hidden">
-        <x-mobile-bottom-nav-button content="Question Requests" />
-        <x-mobile-bottom-nav-button content="Teacher Requests" />
-        <x-mobile-bottom-nav-button content="Feedbacks" />
-    </div> --}}
 </main>
 
 @include('../partials.scripts')
 @stack('scripts')
-
 </body>
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,20 +1,6 @@
 @extends('components.layouts.account')
 
-@php
-$content = [
-    ["Stat 1", "#"], 
-    ["Stat 2", "#"]
-];
-@endphp
-
-@section('sidebar')
-<x-sidebar title="Content" :content="$content" />
-@endsection
-
 @section('content')
 <div class='vue-router'>
 </div>
-@endsection
-@section('mobile-bottom-nav')
-    <x-mobile-bottom-nav :content="$content"/>
 @endsection
