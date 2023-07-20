b22982128150b8fdd656a8474ceca745114fd93d
Mohamed Aymn <mohamedaymn@192.168.1.12>
2023-07-20 16:06:50 +0300

    refactor: profile mobile sidebar

--- resources/js/components/organisms/ProfileSidebar.vue
+++ resources/js/components/organisms/ProfileSidebar.vue
@@ -1,36 +1,54 @@
 <script>
-
-export default{
-    data() {
-        return {
-        elements: [
-            {
-                title: "Dashboard elements",
-                content: [
-                    {
+    export default {
+        data() {
+            return {
+                elements: [{
+                    title: "Dashboard elements",
+                    content: [{
                         linkText: "element one",
                         linkId: "elementOne"
-                    }
-                ]
+                    }]
+                }]
             }
-        ]
         }
     }
-}
 </script>
 
 
 <template>
-<div class="absolute flex-col justify-between hidden w-64 p-6 shadow bg-gray-50 sm:relative sm:h-full sm:flex">
+    <div class="drawer absolute top-1/2 z-50 right-4 lg:hidden">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+            <!-- Page content here -->
+            <label for="my-drawer" class="btn btn-circle drawer-button">></label>
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer" class="drawer-overlay"></label>
+            <ul class="menu p-4 w-80 h-full bg-white text-base-content">
+                <!-- Sidebar content here -->
+                <div v-for="element in elements">
+                    <div class="text-gray-400">{{ element . title }}</div>
+
+                    <div v-for="link in element.content">
+                        <a class="block ml-2 link link-hover" :href="'#' + link.linkId">
+                            {{ link . linkText }}
+                        </a>
+                    </div>
+                </div>
+
+            </ul>
+        </div>
+    </div>
+    <div class="absolute flex-col justify-between hidden w-64 p-6 shadow bg-gray-50 sm:relative sm:h-full lg:flex">
         <div v-for="element in elements">
-            <div class="text-gray-400">{{ element.title }}</div>
-            
+            <div class="text-gray-400">{{ element . title }}</div>
+
             <div v-for="link in element.content">
                 <a class="block ml-2 link link-hover" :href="'#' + link.linkId">
-                    {{ link.linkText }}
+                    {{ link . linkText }}
                 </a>
             </div>
         </div>
         <a href="/auth/logout" class="link link-hover">logout</a>
-</div>
+    </div>
 </template>
