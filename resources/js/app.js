fce82852faf0e4e83ee8c93de1409f1e13e6107c
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-07-20 16:34:50 +0300

    feat: dropdown global function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,22 @@
-import {createApp} from 'vue'
-import ProgressBar from './components/molecules/ProgressBar.vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import ProgressBar from "./components/molecules/ProgressBar.vue";
+import { createPinia } from "pinia";
 // --------------- laravel bootstrap
 import "./bootstrap";
+import { autoCloseDropdown } from "./globalFunctions";
 
 // --------------- vue configuration
 // state management tools
-const pinia = createPinia()
+const pinia = createPinia();
 // vue app
-const app = createApp({})
+const app = createApp({});
 
 // register components
-app.component('progress-bar',ProgressBar)
+app.component("progress-bar", ProgressBar);
 
 // invoke app
-app.use(pinia)
-app.mount("#app")
+app.use(pinia);
+app.mount("#app");
+
+// UI global functions
+autoCloseDropdown();
--- resources/js/globalFunctions.js
+++ resources/js/globalFunctions.js
@@ -1,14 +1,27 @@
-export function changeRoute(route){
-    window.location.href = `http://127.0.0.1:8000/${route}`
+export function changeRoute(route) {
+    window.location.href = `http://127.0.0.1:8000/${route}`;
 }
 
-export function getToken(){
-    const url = '/api/v1/users/tokens'; 
-    axios.get(url)
-        .then(response => {
+export function getToken() {
+    const url = "/api/v1/users/tokens";
+    axios
+        .get(url)
+        .then((response) => {
             // token store sccussfully
         })
-        .catch(error => {
+        .catch((error) => {
             console.error(error);
         });
+}
+
+export function autoCloseDropdown() {
+    $(document).on("click", function (event) {
+        // Check if the clicked element is a dropdown or its child
+        if (!$(event.target).closest(".dropdown").length) {
+            // Loop through each dropdown and remove the open attribute to close them
+            $(".dropdown").each(function () {
+                $(this).removeAttr("open");
+            });
+        }
+    });
 }
