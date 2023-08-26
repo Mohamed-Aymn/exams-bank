06f5aec5c433a7294bd8de5f2553865b511584e5
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-08-26 16:25:16 +0300

    feat: form validation

--- resources/views/customizeExam.blade.php
+++ resources/views/customizeExam.blade.php
@@ -6,7 +6,7 @@
             @csrf
             <div class="form-control">
                 <label class="label">Subject</label>
-                <select class="select font-normal" name='subject' id="subjecet" required>
+                <select class="select font-normal" name='subject' id="subject" >
                     <option disabled selected>Select subject</option>
                     <option value="Physics">Physics</option>
                 </select>
@@ -15,8 +15,8 @@
             <div class="form-control">
                 <label class="label">Time</label>
                 <div class="join">
-                    <input class='input join-item' placeholder="hours" name="hours" required />
-                    <input class='input join-item' placeholder="minutes" name="minutes" required/>
+                    <input class='input join-item' placeholder="hours" id="hours" name="hours"  />
+                    <input class='input join-item' placeholder="minutes" id="minutes" name="minutes" />
                 </div>
             </div>
 
@@ -28,16 +28,16 @@
                         <div class="question-index font-light ml-1">Group 1</div>
                         <div class='question-group join grid grid-cols-12'>
                             <div class="form-control join-item col-span-4">
-                                <select class="select font-normal" name="type[]" required>
+                                <select class="select font-normal" name="type[]"  id="type">
                                     <option disabled selected>Select Type</option>
                                     <option value="1">MCQ</option>
                                 </select>
                             </div>
 
-                            <input class='input join-item col-span-4' placeholder="Number" name="number[]" type='number' required />
+                            <input class='input join-item col-span-4' placeholder="Number" name="number[]" id="number" type='number'  />
 
                             <div class="form-control join-item col-span-4">
-                                <select class="select font-normal" name="level[]" required>
+                                <select class="select font-normal" id="level" name="level[]" >
                                     <option disabled selected class="">Level</option>
                                     <option value="1">One</option>
                                     {{-- <option>Two</option> --}}
@@ -53,11 +53,17 @@
             <button class="btn btn-outline mt-4 w-full" disabled type="button" id="add-group-btn">
                 Add Questions group
             </button>
+
+            <div class="alert alert-error hidden" id="errorContainer">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span id="errorMessages"></span>
+            </div>
+
             <div class="flex w-full justify-between mt-auto">
                 <button class="btn btn-ghost">Cancel</button>
                 <div class="flex gap-2">
                     <button class="btn btn-ghost">save preference</button>
-                    <button class="btn btn-primary" type="submit">Start</button>
+                    <button class="btn btn-primary" id="submitBtn" type="submit">Start</button>
                 </div>
             </div>
         </form>
@@ -107,5 +113,70 @@
                 }
             }
         });
+
+        // form validation
+        let subject = document.querySelector('#subject');
+        let hours = document.querySelector('#hours');
+        let minutes = document.querySelector('#minutes');
+        let type = document.querySelector('#type');
+        let number = document.querySelector('#number');
+        let level = document.querySelector('#level');
+        // error message div
+        let errorContainer = document.querySelector('#errorContainer');
+
+        // submit button
+        let submitBtn = document.querySelector('#submitBtn');
+        
+        let errorMessage = "";
+
+        
+        submitBtn.addEventListener('click', (event) => {
+            // form validation
+            if(subject.value == 'Select subject'){
+                // event.preventDefault();
+                errorMessage += "Subject should be selected \n";
+            }
+
+            if(
+                hours.value > 1 ||
+                minutes.value > 59 ||
+                ( hours.value == "" && minutes.value == "") ||
+                ( hours.value == 0 && minutes.values == 0)
+            ){
+                errorMessage += "Time should be not more that one hour, minutes shouldn't be more than 59 \n"
+            }
+
+            if(
+                // type validation
+                type.value == "Select Type"
+            ){
+                // event.preventDefault();
+                errorMessage +=  "Type shouldn't be left empty \n"
+            }
+
+            if (
+                number.value == "" ||
+                number.value > 25
+            ){
+                // event.preventDefault();
+                errorMessage += "Number shouldn't be left empty, and shouldn't be more than 25 \n"
+            }
+
+            if(
+                level.value == "Level"
+            ){
+                // event.preventDefault();
+                errorMessage += "Level shouldn't be left empty \n"
+            }
+
+            // error message tage
+            if(errorMessage != ''){
+                event.preventDefault()
+                errorContainer.classList.remove("hidden");
+                document.querySelector('#errorMessages').innerText = errorMessage
+            }
+
+            errorMessage = ""
+        })
     </script>
 @endpush
